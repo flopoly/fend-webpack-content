28f7845e1ec9061bfd5540a519a896c9c7876364
= <=>
2023-01-28 20:59:21 +0100

    Before Service Workers

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,31 +1,38 @@
-const path = require("path")
-const webpack = require("webpack")
-const HtmlWebPackPlugin = require("html-webpack-plugin")
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const MiniCSSExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = {
-	mode: "production",
-	entry: "./src/client/index.js",
-	output: {
-		libraryTarget: "var",
-		library: "Client"
-	},
-	module: {
-            rules: [
-                {
+    mode: 'production',
+    entry: './src/client/index.js',
+    optimization: {
+        minimizer: [new TerserPlugin({}), new OptimizeCSSAssetsPlugin({})],
+    },
+    output: {
+        libraryTarget: 'var',
+        library: 'Client',
+    },
+    module: {
+        rules: [
+            {
                 test: /\.js$/,
                 exclude: /node_modules/,
-                loader: "babel-loader"
-                },
-				{
-				test: /\.scss$/,
-				use: [ "style-loader", "css-loader", "sass-loader" ]
-				}
-            ]
+                loader: 'babel-loader',
+            },
+            {
+                test: /\.scss$/,
+                use: [MiniCSSExtractPlugin.loader, 'css-loader', 'sass-loader'],
+            },
+        ],
     },
-	plugins: [
-		new HtmlWebPackPlugin({
-			template: "./src/client/views/index.html",
-			filename: "./index.html",
-		})
-	]
-}
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: './src/client/views/index.html',
+            filename: './index.html',
+        }),
+        new MiniCSSExtractPlugin({filename: '[name].css'}),
+    ],
+};
